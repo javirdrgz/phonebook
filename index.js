373c91a7b3629417e2e7e08eb994beab2efd5539
Javi <javirod.cba@gmail.com>
2021-07-01 09:10:52 +1000

    define PORT to an environmental variable

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+
+morgan.token("reqBody", (req, _) => JSON.stringify(req.body));
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :reqBody"
+  )
+);
 
 let persons = [
   {
@@ -26,7 +36,66 @@ let persons = [
   },
 ];
 
-const PORT = 3001;
+const generateId = () => {
+  return Number(Math.random().toString().slice(2));
+};
+
+app.get("/", (_, res) => {
+  res.status(200).send("<h1>Hello World</h1>");
+});
+
+app.get("/info", (_, res) => {
+  const info = `
+          <p>Phonebook has info for ${persons.length} people</p>
+          <p>${new Date()}</p>
+    `;
+  res.status(200).send(info);
+});
+
+app.get("/api/persons", (_, res) => {
+  res.status(200).json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  person ? res.json(person) : res.status(404).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const { name, number } = req.body;
+
+  if (!number) return res.status(400).json({ error: "missing number" });
+  if (!name) return res.status(400).json({ error: "missing name" });
+
+  const nameIsNotUnique = persons.some((p) => p.name === name);
+
+  if (nameIsNotUnique)
+    return res.status(400).json({ error: "name must be unique" });
+
+  const person = {
+    name,
+    number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
+
+const unknownEndpoint = (_, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
