2b001f21f4b41469f8bd89313568908f7b5b2303
Javi <javirod.cba@gmail.com>
2021-07-02 19:19:08 +1000

    Add Person model

---
+++ models/person.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+url = process.env.MONGODB_URI;
+
+console.log("connecting to database");
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to MongoDB"))
+  .catch((error) =>
+    console.log("failed to connect to database", error.message)
+  );
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
