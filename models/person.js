4352b40434c167734f639e10e4dfb5e5700de349
Javi <javirod.cba@gmail.com>
2021-07-03 12:25:45 +1000

    Add validation to Person schema

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 url = process.env.MONGODB_URI;
 
@@ -17,9 +18,18 @@ mongoose
   );
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
 });
+
+personSchema.plugin(uniqueValidator);
 
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
